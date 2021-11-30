ddd191306af41a7409d6918d247322e36eb03a0c
RosalRicardo <ricardorosal@archdesktop1.localdomain>
2021-11-29 22:24:55 -0300

    rebase

--- src/multiple_runs.py
+++ src/multiple_runs.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import torch
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import f1_score
